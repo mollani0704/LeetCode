7366b852e1ef9f981db4b1b1e3dfd7a32cb5dbc1
JongSeong Moon <72548305+mollani0704@users.noreply.github.com>
2024-05-14 17:11:26 +0900

    Time: 34 ms (57.14%), Space: 13.7 MB (10.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,38 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        s = sorted(list(s))
-        t = sorted(list(t))
 
-        if(len(s) != len(t)):
+        # HashMap을 이용한 정답
+        if len(s) != len(t):
             return False
+        
+        countS, countT = {}, {}
 
-        count = 0
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
 
-        for i in range(0, len(s)):
-            if(s[i] == t[i]):
-                count = count + 1
-            else: 
-                count = 0
-        if(count == len(s)):
-            return True
-        else:
-            return False
+        # 내가 푼 정답
+        # s = sorted(list(s))
+        # t = sorted(list(t))
+
+        # if(len(s) != len(t)):
+        #     return False
+
+        # count = 0
+
+        # for i in range(0, len(s)):
+        #     if(s[i] == t[i]):
+        #         count = count + 1
+        #     else: 
+        #         count = 0
+        # if(count == len(s)):
+        #     return True
+        # else:
+        #     return False
 
 
     
