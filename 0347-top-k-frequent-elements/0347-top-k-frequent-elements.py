0d7e9d7e04a93fd65975dae827c669efafd3f2d6
JongSeong Moon <72548305+mollani0704@users.noreply.github.com>
2024-05-19 15:11:13 +0900

    Time: 69 ms (82.47%), Space: 14.9 MB (89.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,28 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
-        for n, c in count.items():
-            freq[c].append(n)
+        # 두번째 풀이
+        elements = {}
+        for num in nums:
+            elements[num] = elements.get(num, 0) + 1
         
-        result = []
+        sorted_elements = sorted(elements.items(), key=lambda x : x[1], reverse=True)
 
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                result.append(n)
-                if len(result) == k:
-                    return result
+        top_k = [item[0] for item in sorted_elements[:k]]
+        return top_k
+        
+        # 첫번쨰 풀이
+        # count = {}
+        # freq = [[] for i in range(len(nums) + 1)]
+
+        # for n in nums:
+        #     count[n] = 1 + count.get(n, 0)
+        # for n, c in count.items():
+        #     freq[c].append(n)
+        
+        # result = []
+
+        # for i in range(len(freq) - 1, 0, -1):
+        #     for n in freq[i]:
+        #         result.append(n)
+        #         if len(result) == k:
+        #             return result
