05ed793052f7c88d207c309c508cffd0d7de798b
JongSeong Moon <72548305+mollani0704@users.noreply.github.com>
2024-05-28 22:29:00 +0900

    Time: 83 ms (24.98%), Space: 18 MB (14.56%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        result = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            result[tuple(count)].append(s)
+        
+        return result.values()
+
+        
